723848ba6d6fd2ec9a16e7a2fe48b0bd8a23ce15
KashMihdi <127855730+KashMihdi@users.noreply.github.com>
2023-04-17 22:15:02 +0300

    Initial Commit

---
+++ ContactList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactList
+//
+//  Created by Kasharin Mikhail on 17.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
